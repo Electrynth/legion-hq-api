d4c32b8083f41deb68c2458dc8e30cbea0edb2d0
Nicholas Brown <37748722+NicholasCBrown@users.noreply.github.com>
2019-10-23 12:47:45 -0700

    Updating user routes
    Find user by email or userId

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 module.exports = (app, autoIncrement) => {
   const users = require('../controllers/user.controller.js');
   // TODO: app.get('/users', users.findAll);
-  app.get('/users/:userId', users.findOne);
+  app.get('/users', users.findByEmail);
+  app.get('/users/:userId', users.findByUserId);
   app.post('/users', users.create);
   // TODO: app.put('/users/:userId', users.update);
   // TODO: app.delete('/users/:userId', users.delete);
